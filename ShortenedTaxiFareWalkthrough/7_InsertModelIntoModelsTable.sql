5d52bc24ae0dbf2f27b81d18da4fd6e5c29c574f
edfreeman <ed.freeman@endjin.com>
2018-01-11 18:05:50 +0000

    Delete 7_InsertModelIntoModelsTable.sql

--- ShortenedTaxiFareWalkthrough/7_InsertModelIntoModelsTable.sql
+++
@@ -1,7 +0,0 @@
---==============================================
---Insert the trained model into the models table.
---==============================================
-
-DECLARE @model VARBINARY(MAX);
-EXEC TrainTipPredictionModelSciKitPy @model OUTPUT;
-INSERT INTO nyc_taxi_models (name, model) VALUES('SciKit_model', @model);
